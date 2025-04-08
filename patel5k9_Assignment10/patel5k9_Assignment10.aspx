9c044fec9dd7e77f1f3842d97250e7a8dd35f3e1
Kush Patel <patel5k9@mail.uc.edu>
2025-04-08 17:20:44 -0400

    Add project files.

---
+++ patel5k9_Assignment10/patel5k9_Assignment10.aspx
@@ -0,0 +1,75 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="patel5k9_Assignment10.aspx.cs" Inherits="patel5k9_Assignment10.patel5k9_Assignment10" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Virat Kohli - Cricket Legend</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            text-align: center;
+        }
+        header {
+            background-color: blue;
+            color: white;
+            padding: 10px;
+        }
+        .container {
+            width: 80%;
+            margin: 20px auto;
+            background: #fff;
+            padding: 20px;
+            border-radius: 8px;
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin: 20px 0;
+        }
+        th, td {
+            border: 1px solid #ddd;
+            padding: 10px;
+        }
+        th {
+            background-color: #333;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <header>
+            <h1>Virat Kohli - Cricket Legend</h1>
+        </header>
+        <div class="container">
+            <asp:Image ID="Image1" runat="server" ImageUrl="Viratkohli.jpg" Width="300" AlternateText="Virat Kohli" />
+            <p>Virat Kohli is one of the greatest cricketers of all time. He has led the Indian cricket team to many victories and is known for his aggressive playing style and consistency.</p>
+
+            <h2>Career Highlights</h2>
+            <asp:BulletedList ID="HighlightsList" runat="server" BulletStyle="Disc"></asp:BulletedList>
+
+            <h2>Statistics</h2>
+            <asp:GridView ID="StatsGrid" runat="server" AutoGenerateColumns="False" ShowHeader="true">
+                <Columns>
+                    <asp:BoundField HeaderText="Format" DataField="Format" />
+                    <asp:BoundField HeaderText="Matches" DataField="Matches" />
+                    <asp:BoundField HeaderText="Runs" DataField="Runs" />
+                    <asp:BoundField HeaderText="Average" DataField="Average" />
+                </Columns>
+            </asp:GridView>
+            
+            <h2>Inspirational Quote</h2>
+            <blockquote>
+                "Self-belief and hard work will always earn you success." – Virat Kohli
+            </blockquote>
+
+            <p>
+                Learn more about Virat Kohli on his
+                <asp:Button ID="ButtonICC" runat="server" Text="View ICC Profile" OnClick="ButtonICC_Click" />
+            </p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ patel5k9_Assignment10/patel5k9_Assignment10.aspx.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * File Name: patel5k9_Assignment 10
+* Name:Kush Patel
+* email: Patel5k9@mail.uc.edu
+* Assignment Number: Assignment 10
+* Due Date:   4/10/2025
+* Course #/Section: IS3050 001
+* Semester/Year:   Spring 2025
+* Brief Description of the assignment: This assignment involves Re-create my submission from Assignment 02 as an ASP.Net web page in .aspx format. Using ASP.Net web controls where appropriate to replace the standard HTML controls I originally used.
+* Brief Description of what this module does: This module This module focuses on showcasing the understanding of ASP.NET Web Forms page with images, statistics, and interactive elements.
+* Citations: https://www.youtube.com/watch?v=cLr4_CgWqPM&ab_channel=VereMiller (to learn how to add IMG on a Mac)
+ */
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace patel5k9_Assignment10
+{
+    public partial class patel5k9_Assignment10 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+                LoadHighlights();
+                LoadStatistics();
+            
+        }
+            private void LoadHighlights()
+            {
+                HighlightsList.Items.Add(new ListItem("Former Captain of the Indian Cricket Team"));
+                HighlightsList.Items.Add(new ListItem("Over 70 International Centuries"));
+                HighlightsList.Items.Add(new ListItem("Winner of the ICC Cricketer of the Year Award"));
+                HighlightsList.Items.Add(new ListItem("Key player in India's 2011 World Cup Victory"));
+            }
+
+            private void LoadStatistics()
+            {
+                DataTable dt = new DataTable();
+                dt.Columns.Add("Format");
+                dt.Columns.Add("Matches");
+                dt.Columns.Add("Runs");
+                dt.Columns.Add("Average");
+
+                dt.Rows.Add("Test", "111", "8676", "49.5");
+                dt.Rows.Add("ODI", "265", "12300", "57.5");
+                dt.Rows.Add("T20", "115", "4008", "52.7");
+
+                StatsGrid.DataSource = dt;
+                StatsGrid.DataBind();
+            }
+            protected void LinkButtonICC_Click(object sender, EventArgs e)
+            {
+                Response.Redirect("https://www.icc-cricket.com/rankings/mens/player-rankings/164");
+            }
+
+            protected void ButtonICC_Click(object sender, EventArgs e)
+            {
+                Response.Redirect("https://www.icc-cricket.com/rankings/mens/player-rankings/164");
+            }
+
+
+    }
+}
